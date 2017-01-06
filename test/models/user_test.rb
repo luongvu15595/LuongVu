c7271b6d6cbc15e2e8b7ae534afd6b0d1cc76c6e
luongvu15595 <luongvu15595>
2017-01-06 11:40:13 +0700

    sign-up3

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -33,7 +33,7 @@ class UserTest < ActiveSupport::TestCase
   end
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn]
+      first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
